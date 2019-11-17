22ba34251f0efdd68cfef7603ce104f6cf9c6747
Thomas Jakway <tjakway@nyu.edu>
2019-11-17 14:41:38 -0500

    Outline of property-setting backend classes

--- src/main/scala/com/jakway/sqlpp/Backend.scala
+++ src/main/scala/com/jakway/sqlpp/Backend.scala
@@ -1,14 +1,44 @@
 package com.jakway.sqlpp
 
-case class Backend(names: Set[String]) {
+import java.io.InputStream
+import java.util.Properties
+
+import com.jakway.sqlpp.Config.ConfigError
+import com.jakway.sqlpp.error.SqlppError
+
+abstract class Backend(val names: Set[String]) {
   def matches(x: String): Boolean =
     names.exists(Backend.areEqual(_, x))
+
+  def setEngineProperties(velocityProperties: Properties): Unit
+}
+
+abstract class NamedPropertiesBackend(override val names: Set[String])
+  extends Backend(names) {
+
+  override def setEngineProperties(velocityProperties: Properties): Unit = {
+
+  }
+
+
 }
 
 object Backend {
+  class BackendLoadError(override val causes: Seq[SqlppError])
+    extends ConfigError(causes)
+
+  class OpenStreamError(override val msg: String)
+    extends SqlppError(msg)
+
   def areEqual(left: String, right: String): Boolean = {
     def normalizeString(str: String): String = str.trim.toLowerCase()
 
     normalizeString(left) == normalizeString(right)
+  }
+
+  case class ClassResourceBackend(override val names: Set[String],
+                                  resourcePath: String)
+    extends Backend(names) {
+
   }
 }
