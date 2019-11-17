a346cc3d21a4d1a28b53050ce18e185f1a43e113
Thomas Jakway <tjakway@nyu.edu>
2019-11-17 14:51:03 -0500

    using velocity resource loader, added MapToProperties util class

---
+++ src/main/scala/com/jakway/sqlpp/util/MapToProperties.scala
@@ -0,0 +1,28 @@
+package com.jakway.sqlpp.util
+
+import java.util.Properties
+
+import scala.util.Try
+
+object MapToProperties {
+  /**
+   * returns a map of overwritten values (i.e. the key-value pairs
+   * where the key was already present in the map before this method call)
+   * @param kvs
+   * @param prop
+   * @return
+   */
+  def apply(kvs: Map[String, String])
+           (prop: Properties): Try[Map[String, String]] = Try {
+
+    val empty: Map[String, String] = Map()
+    kvs.foldLeft(empty) {
+      case (prevValues, (key, value)) => {
+        Option(prop.setProperty(key, value)) match {
+          case Some(oldValue) => prevValues.updated(key, oldValue)
+          case None => prevValues
+        }
+      }
+    }
+  }
+}
