7ec439fa65e83970543cc13c7d904f106dbfe53f
Thomas Jakway <tjakway@nyu.edu>
2019-11-27 16:02:25 -0800

    fixed ValueSource interopt with VelocityContext

--- common/src/main/scala/com/jakway/sqlpp/template/ValueSource.scala
+++ common/src/main/scala/com/jakway/sqlpp/template/ValueSource.scala
@@ -1,14 +1,11 @@
 package com.jakway.sqlpp
 
 import com.jakway.sqlpp.error.SqlppError
-
 import com.jakway.sqlpp.util.{ContextUtil, MapToProperties}
-
 import org.apache.velocity.VelocityContext
 import java.util.Properties
 
-
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success, Try}
 
 sealed trait ValueSource {
   def toVelocityContext: Either[SqlppError, VelocityContext]
@@ -37,7 +34,18 @@ case class PropertySource(prop: Properties) extends ValueSource {
 
 case class MapSource(map: Map[String, String]) extends ValueSource {
   override def toVelocityContext: Either[SqlppError, VelocityContext] = {
-    MapToProperties.withNewProperties(map) match {
+    import java.util.{Map => JMap}
+    import scala.collection.JavaConverters
+
+    val convMap: JMap[String, Object] = {
+      val objMap = map.map {
+        case (key, value) => (key, value: Object)
+      }
+
+      JavaConverters.mapAsJavaMap(objMap)
+    }
+
+    Try(new VelocityContext(convMap)) match {
       case Success(x) => Right(x)
       case Failure(t) => Left(ValueSource.ValueSourceError(t))
     }
