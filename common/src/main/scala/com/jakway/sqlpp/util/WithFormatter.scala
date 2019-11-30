900f1f05dea64c4383854a27fbce64cecd25b89b
Thomas Jakway <tjakway@nyu.edu>
2019-11-30 12:03:39 -0800

    fixed varargs passing in WithFormatter, implemented WithPathSeparator

--- common/src/main/scala/com/jakway/sqlpp/util/WithFormatter.scala
+++ common/src/main/scala/com/jakway/sqlpp/util/WithFormatter.scala
@@ -1,6 +1,11 @@
 package com.jakway.sqlpp.util
 
+import java.io.File
 import java.util.Formatter
+
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.util.matching.Regex
 
 object WithFormatter {
   private def mkFormatter: Formatter = {
@@ -11,7 +16,33 @@ object WithFormatter {
   def apply(formatStr: String, args: Object*): String = {
     val fmt: Formatter = mkFormatter
 
-    fmt.format(formatStr, args)
+    fmt.format(formatStr, args: _*)
     fmt.toString
+  }
+
+  object WithPathSeparator {
+    private val formatStrRegex: Regex = """(?<!\\\\)%s""".r
+    val logger: Logger = LoggerFactory.getLogger(getClass)
+
+    //see https://stackoverflow.com/questions/5971964/file-separator-or-file-pathseparator
+    //regarding which field to use
+    val defaultPathSeparator: String = File.separator
+
+    private def countFormatStrings(str: String): Int = {
+      formatStrRegex.findAllIn(str).length
+    }
+
+    def apply(formatStr: String,
+              useSeparator: String = defaultPathSeparator): String = {
+
+      //count the number of times %s appears in the formatStr
+      //then make a Seq of that length
+      val replaceArgs: Seq[String] =
+        List.fill(countFormatStrings(formatStr))(useSeparator)
+
+      logger.debug(s"Processing < $formatStr > with $replaceArgs")
+
+      WithFormatter.apply(formatStr, replaceArgs: _*)
+    }
   }
 }
