8971625984c5494d18600b90ba1f5c7d0f3a8501
Thomas Jakway <tjakway@nyu.edu>
2019-12-06 16:19:13 -0500

    implemented PrioritizedPref

--- main/src/main/scala/com/jakway/sqlpp/config/PrioritizedPref.scala
+++ main/src/main/scala/com/jakway/sqlpp/config/PrioritizedPref.scala
@@ -1,9 +1,38 @@
 package com.jakway.sqlpp.config
 
+import com.jakway.sqlpp.config.Config.ConfigError
+import com.jakway.sqlpp.config.PrioritizedPref.NoPrefFoundError
 import com.jakway.sqlpp.error.SqlppError
 
-class PrioritizedPref[A](val orderedGetters: Seq[() => Either[SqlppError, A]]) {
+class PrioritizedPref[A](val orderedGetters: Seq[() => Either[SqlppError, A]],
+                         val default: Option[A] = None) {
   def apply(): Either[SqlppError, A] = {
-    ???
+    val empty: (Seq[SqlppError], Option[A]) = (Seq.empty, None)
+    val res = orderedGetters.foldLeft(empty) {
+      case ((errors, None), thisGetter) => {
+        thisGetter() match {
+          case Right(x) => (errors, Some(x))
+          case Left(e) => (errors :+ e, None)
+        }
+      }
+      case ((errors, Some(x)), _) => (errors, Some(x))
+    }
+
+    res match {
+      case (_, Some(x)) => Right(x)
+      case (errors, None) => {
+        default match {
+            //use the default if no getters returned values
+          case Some(x) => Right(x)
+            //if there is no default it's an error
+          case None => Left(new NoPrefFoundError(errors))
+        }
+      }
+    }
   }
+}
+
+object PrioritizedPref {
+  class NoPrefFoundError(val errors: Seq[SqlppError])
+    extends ConfigError(errors)
 }
