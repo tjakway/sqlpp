e7bf05d3121452864941282036258b21a028f819
Thomas Jakway <tjakway@nyu.edu>
2019-12-06 17:00:08 -0500

    implemented StdoutOutputTarget

--- main/src/main/scala/com/jakway/sqlpp/config/OutputTarget.scala
+++ main/src/main/scala/com/jakway/sqlpp/config/OutputTarget.scala
@@ -1,5 +1,52 @@
 package com.jakway.sqlpp.config
 
-import com.jakway.sqlpp.Backend
+import java.io.{BufferedWriter, File, OutputStreamWriter, Writer}
 
-class OutputTarget(val backend: Backend)
+import com.jakway.sqlpp.Backend
+import com.jakway.sqlpp.config.OutputTarget.OpenWriterError
+import com.jakway.sqlpp.config.error.ConfigError
+import com.jakway.sqlpp.error.SqlppError
+import com.jakway.sqlpp.util.FileUtil
+
+import scala.util.{Try, Success, Failure}
+
+abstract class OutputTarget(val backend: Backend) {
+  def getWriter(encoding: String): Either[SqlppError, Writer]
+}
+
+object OutputTarget {
+  class OutputTargetError(override val msg: String)
+    extends ConfigError(msg)
+
+  class OpenWriterError(override val msg: String)
+    extends OutputTargetError(msg) {
+    def this(throwable: Throwable) {
+      this(SqlppError.formatThrowable(throwable))
+    }
+  }
+
+  object OpenWriterError {
+    def fromTry(f: => Try[Writer]): Either[SqlppError, Writer] = {
+      f match {
+        case Success(w) => Right(w)
+        case Failure(t) => Left(new OpenWriterError(t))
+      }
+    }
+  }
+}
+
+class FileOutputTarget(override val backend: Backend,
+                       val dest: File)
+  extends OutputTarget(backend) {
+
+  override def getWriter(encoding: String): Either[SqlppError, Writer] =
+    FileUtil.openWriter(encoding, new OpenWriterError(_))(dest)
+}
+
+class StdoutOutputTarget(override val backend: Backend)
+  extends OutputTarget(backend) {
+
+  override def getWriter(encoding: String): Either[SqlppError, Writer] =
+    OpenWriterError.fromTry(Try(
+      new BufferedWriter(new OutputStreamWriter(System.out))))
+}
