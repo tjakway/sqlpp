3010bd3acd2f05efd0d7a12de3dd0371bb6943a2
Thomas Jakway <tjakway@nyu.edu>
2019-12-07 18:35:51 -0500

    fixup! moved Config.Defaults -> new top-level class Defaults moved defaults-related fields out of DataDir and into new class Defaults

--- main/src/main/scala/com/jakway/sqlpp/config/unchecked/UncheckedConfig.scala
+++ main/src/main/scala/com/jakway/sqlpp/config/unchecked/UncheckedConfig.scala
@@ -2,7 +2,7 @@ package com.jakway.sqlpp.config.unchecked
 
 import java.io.File
 
-import com.jakway.sqlpp.config.checked.Config.Defaults
+import com.jakway.sqlpp.config.Defaults
 import com.jakway.sqlpp.template.ResourceLoaderConfig.StandardResourceLoaders.LoaderType
 
 case class UncheckedConfig(source: Option[File] = None,
