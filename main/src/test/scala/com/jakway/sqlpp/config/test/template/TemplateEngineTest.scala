a3ad959437d404e90a36b2c39dde5ed4af9d9941
Thomas Jakway <tjakway@nyu.edu>
2019-12-21 12:03:53 -0500

    writing TemplateEngineTest

---
+++ main/src/test/scala/com/jakway/sqlpp/config/test/template/TemplateEngineTest.scala
@@ -0,0 +1,18 @@
+package com.jakway.sqlpp.config.test.template
+
+import com.jakway.sqlpp.config.test.WithDefaultTestConfig
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+abstract class TemplateEngineTest(val testResource: String,
+                                  val testName: String = getClass.getName)
+  extends AnyFlatSpec
+    with Matchers
+    with TemplateEngineTestAsserter
+    with WithDefaultTestConfig {
+
+  testName should "pass template engine tests" in {
+
+  }
+
+}
