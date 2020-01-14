6efaa00f4bfa599f3ea8e42954b6f5e8e72efb98
Thomas Jakway <tjakway@nyu.edu>
2020-01-14 15:35:27 -0500

    changed Main to use Run.Result

--- exe/src/main/scala/com/jakway/sqlpp/Main.scala
+++ exe/src/main/scala/com/jakway/sqlpp/Main.scala
@@ -1,7 +1,15 @@
 package com.jakway.sqlpp
 
+import com.jakway.sqlpp.Run.Result
+
 object Main {
   def main(args: Array[String]): Unit = {
-    Run.apply(args)
+    Run.apply(args) match {
+        //print any error messages to stderr and exit
+      case Result(exitCode, errorMessage) => {
+        errorMessage.foreach(System.err.print)
+        System.exit(exitCode)
+      }
+    }
   }
 }
--- main/src/main/scala/com/jakway/sqlpp/Run.scala
+++ main/src/main/scala/com/jakway/sqlpp/Run.scala
@@ -4,8 +4,31 @@ import com.jakway.sqlpp.config.checked
 import com.jakway.sqlpp.error.SqlppError
 
 object Run {
-  def apply(args: Array[String]): Unit = {
-    //TODO: parse config
+  case class Result(exitCode: Int, errorMessage: Option[String]) {
+    def success: Boolean = exitCode == Result.successCode
+  }
+
+  object Result {
+    val successCode: Int = 0
+  }
+
+  def printErrorMessage(sqlppError: SqlppError): String = {
+    val fmt = new java.util.Formatter(new StringBuffer())
+
+    fmt.format("Error, message follows\n%s\n", sqlppError.print)
+    fmt.toString
+  }
+
+  //TODO: parse config
+  private def parse(args: Array[String]):
+    Either[error.SqlppError, checked.Config] = ???
+
+  def apply(args: Array[String]): Result = {
+    parse(args).flatMap(apply) match {
+      case Right(_) => Result(Result.successCode, None)
+      case Left(error) =>
+        Result(1, Some(printErrorMessage(error)))
+    }
   }
 
   def apply(checkedConfig: checked.Config): Either[SqlppError, Unit] = {
