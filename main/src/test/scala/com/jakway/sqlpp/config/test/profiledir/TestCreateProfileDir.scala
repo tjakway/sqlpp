a6bfb3817032b814a7a94e2d68d7d5dbc5dfd92f
Thomas Jakway <tjakway@nyu.edu>
2020-02-27 17:41:40 -0500

    fixup! fixup! moved WithTempDir to its own file in new package framework

--- main/src/test/scala/com/jakway/sqlpp/config/test/profiledir/TestCreateProfileDir.scala
+++ main/src/test/scala/com/jakway/sqlpp/config/test/profiledir/TestCreateProfileDir.scala
@@ -1,10 +1,11 @@
 package com.jakway.sqlpp.config.test.profiledir
 
 import java.io.File
+import java.nio.file.Files
 
 import com.jakway.sqlpp.config.entries.ParseOutputPattern
 import com.jakway.sqlpp.config.output.StdoutOutputPattern
-import com.jakway.sqlpp.config.test.WithDefaultTestConfig
+import com.jakway.sqlpp.config.test.{GenTestConfig, TestConfig, WithDefaultTestConfig}
 import com.jakway.sqlpp.config.test.error.TestError
 import com.jakway.sqlpp.config.test.gen.GenUtil
 import com.jakway.sqlpp.config.test.profiledir.CreateProfileDirProperties.CreateProfileDirTestException
@@ -28,7 +29,8 @@ object CreateProfileDirProperties {
   class CreateProfileDirTestException(val msg: String)
     extends RuntimeException(msg)
 
-  abstract class CreateProfileDirTest(val backends: Set[Backend],
+  abstract class CreateProfileDirTest(val profDir: File,
+                                      val backends: Set[Backend],
                                       val encoding: String,
                                       val deleteOnFailure: Boolean) {
     def checkOutcome: File =>
@@ -36,10 +38,12 @@ object CreateProfileDirProperties {
                       Either[SqlppError, Unit]
   }
 
-  class CreateProfileDirSuccessTest(override val backends: Set[Backend],
+  class CreateProfileDirSuccessTest(override val profDir: File,
+                                    override val backends: Set[Backend],
                                     override val encoding: String,
                                     override val deleteOnFailure: Boolean)
-    extends CreateProfileDirTest(backends, encoding, deleteOnFailure) {
+    extends CreateProfileDirTest(profDir,
+      backends, encoding, deleteOnFailure) {
 
     override def checkOutcome: File =>
       Either[SqlppError, Unit] =>
@@ -49,9 +53,43 @@ object CreateProfileDirProperties {
 
           for {
             _ <- createProfileDirRes
-
+            //TODO
           } yield {}
       }
+    }
+  }
+
+  val defaultGenProfileDirPrefix: String = "genprofdir"
+
+  def genCreateProfileDirSuccessTest(tempDir: File,
+                                     prefix: String =
+                                       defaultGenProfileDirPrefix,
+                                     genEncoding: Gen[String] =
+                                      GenTestConfig.genEncoding,
+                                     genBackends: Gen[Set[Backend]]):
+    Gen[CreateProfileDirSuccessTest] = {
+
+    def genProfDir: Gen[File] = {
+      val profDir = Files.createTempDirectory(tempDir.toPath, prefix)
+      if(profDir.toFile.exists()) {
+        Files.delete(profDir)
+      }
+
+      assert(!profDir.toFile.exists())
+      profDir.toFile
+    }
+
+
+    val genDeleteOnFailure: Gen[Boolean] = Arbitrary.arbBool.arbitrary
+
+    for {
+      profDir <- genProfDir
+      encoding <- genEncoding
+      backends <- genBackends
+      deleteOnFailure <- genDeleteOnFailure
+    } yield {
+      new CreateProfileDirSuccessTest(
+        profDir, backends, encoding, deleteOnFailure)
     }
   }
 }
