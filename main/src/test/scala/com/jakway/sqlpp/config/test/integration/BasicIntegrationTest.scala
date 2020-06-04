334331009b4cc0a3764ce4b017604f6cfe864d5f
Thomas Jakway <tjakway@nyu.edu>
2020-06-04 13:10:15 -0700

    wrote basic integration test

--- main/src/test/scala/com/jakway/sqlpp/config/test/integration/BasicIntegrationTest.scala
+++ main/src/test/scala/com/jakway/sqlpp/config/test/integration/BasicIntegrationTest.scala
@@ -1,8 +1,14 @@
 package com.jakway.sqlpp.config.test.integration
 
 import java.io.File
+import java.util.Formatter
 
+import com.jakway.sqlpp
+import com.jakway.sqlpp.Run
+import com.jakway.sqlpp.config.test.error.TestError
 import com.jakway.sqlpp.config.test.framework.{TestFileUtils, WithTempDirBeforeAndAfter}
+import com.jakway.sqlpp.config.test.integration.BasicIntegrationTest.UnexpectedOutputError
+import com.jakway.sqlpp.error.{CheckFile, SqlppError}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.slf4j.{Logger, LoggerFactory}
@@ -14,11 +20,12 @@ class BasicIntegrationTest
     with TestFileUtils{
   val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  def args(input: File, outputDest: File) = Seq(
+  def mkArgs(input: File, outputDest: File): Array[String] = Array(
     "--no-create-profile-dir",
     "--resource-loader-types", "file,class",
-    "--source", input,
-    "--output", outputDest.getAbsolutePath
+    "--source", input.getAbsolutePath,
+    "--output", outputDest.getAbsolutePath,
+    "--target-backends", "defaults.xml"
   )
 
   val input: String = "CREATE TABLE Users(" +
@@ -48,14 +55,107 @@ class BasicIntegrationTest
     }
   }
 
+  def checkOutput(outputDest: File,
+                  expectedOutput: String): Either[SqlppError, Unit] = {
+    readEntireFile(outputDest).flatMap { contents =>
+
+      if(contents == expectedOutput) {
+        Right({})
+      } else {
+        Left(new UnexpectedOutputError(contents, expectedOutput))
+      }
+    }
+  }
+
+  private val inputFileChecks = Seq(
+    CheckFile.checkExists,
+    CheckFile.checkIsFile,
+    CheckFile.checkReadable,
+    CheckFile.checkNonEmpty
+  )
+
+  private val outputFileChecks = inputFileChecks
 
 
   "Sqlpp" should "run" in {
+    val td = getTempDir
 
+    val inputDest: File = new File(td, "input.txt")
+    val outputDest: File = new File(td, "output.txt")
+    val expectedOutput = mkExpectedOutput(replacementMap)(input)
+
+    inputDest.exists() shouldEqual false
+    outputDest.exists() shouldEqual false
+
+    val r = for {
+      _ <- writeToFile(input, inputDest)
+      _ <- CheckFile.composeAll(inputFileChecks)(inputDest)
+      res <- sqlpp.Run.applyEither(mkArgs(inputDest, outputDest))
+      _ <- CheckFile.composeAll(outputFileChecks)(outputDest)
+      _ <- checkOutput(outputDest, expectedOutput)
+    } yield {
+      res
+    }
+
+    r should be ('right)
+    //TODO: check exit code
   }
-
 }
 
 object BasicIntegrationTest {
+  class BasicIntegrationTestError(override val msg: String)
+    extends TestError(msg)
 
+  class UnexpectedOutputError(val actual: String, val expected: String)
+    extends TestError(
+      UnexpectedOutputError.formatMsg(
+        "BasicIntegrationTests")(actual, expected))
+
+
+  object UnexpectedOutputError {
+    private val leftCharacter: String = ">"
+
+    private def formatLines(fmt: Formatter)(x: String): Unit = {
+      if(x.isEmpty) {
+        fmt.format("<blank string>\n")
+      } else if(!x.isEmpty && x.trim.isEmpty) {
+        fmt.format(s"<string consists of ${x.length} whitespace characters>\n")
+      } else {
+        import scala.collection.JavaConverters
+        x.lines.toArray.foreach { line =>
+          fmt.format("%s\t%s\n", leftCharacter, line)
+        }
+      }
+    }
+
+    /**
+     * Better error message formatting
+     * Otherwise the messages given can be very long on a small number of lines--
+     * difficult for humans to parse
+     * @param testName
+     * @param actual
+     * @param expected
+     * @return
+     */
+    private def formatMsg(testName: String)
+                         (actual: String, expected: String): String = {
+      val fmt: Formatter = {
+        val sb: StringBuffer = new StringBuffer()
+        new Formatter(sb)
+      }
+
+      fmt.format("Unexpected output in %s\n", testName)
+
+      fmt.format("Expected output:\n")
+      formatLines(fmt)(expected)
+
+      fmt.format("------------------\n")
+
+      fmt.format("Actual output:\n")
+      formatLines(fmt)(actual)
+
+
+      fmt.toString
+    }
+  }
 }
