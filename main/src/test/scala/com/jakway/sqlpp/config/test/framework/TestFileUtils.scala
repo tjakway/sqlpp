d35fc1e59b05d451767b56e2b4a80de9ea05e114
Thomas Jakway <tjakway@nyu.edu>
2020-06-04 13:07:52 -0700

    added readEntireFile, withSource

--- main/src/test/scala/com/jakway/sqlpp/config/test/framework/TestFileUtils.scala
+++ main/src/test/scala/com/jakway/sqlpp/config/test/framework/TestFileUtils.scala
@@ -4,8 +4,10 @@ import java.io.File
 import java.nio.charset.{Charset, StandardCharsets}
 import java.nio.file.Files
 
-import com.jakway.sqlpp.config.test.framework.TestFileUtils.WriteToFileError
-import com.jakway.sqlpp.error.SqlppError
+import com.jakway.sqlpp.config.test.error.TestError
+import com.jakway.sqlpp.config.test.framework.TestFileUtils.{ReadEntireFileError, WriteToFileError}
+import com.jakway.sqlpp.error.{CheckFile, SqlppError}
+import com.jakway.sqlpp.util.TryToEither
 
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
@@ -61,17 +63,59 @@ trait TestFileUtils {
     } yield {}
   }
 
+  /**
+   * safely pass a Source to [[f]] before closing it
+   * @param open
+   * @param f
+   * @tparam A
+   * @return
+   */
+  def withSource[A](open: () => Source)(f: Source => A): A = {
+    var optSource: Option[Source] = None
+    try {
+      val s = open()
+      optSource = Some(s)
+      f(s)
+    } finally {
+      optSource.foreach(_.close())
+    }
+  }
+
+
+  def readEntireFile(f: File,
+                     charset: Charset =
+                       TestFileUtils.defaultCharset):
+    Either[SqlppError, String] = {
+
+    def open() = Source.fromFile(f, charset.displayName())
+
+    def g: Try[String] = Try(withSource(open)(_.mkString))
+
+    for {
+      _ <- CheckFile.checkExists(f)
+      _ <- CheckFile.checkReadable(f)
+      res <- TryToEither(new ReadEntireFileError(f, _))(g)
+    } yield {res}
+  }
 }
 
 object TestFileUtils {
   class WriteToFileError(override val msg: String)
-    extends SqlppError(msg) {
+    extends TestError(msg) {
 
     def this(contents: String,
              dest: File,
              cause: Throwable) {
       this(s"Error writing < $contents > to file < $dest >: " +
         SqlppError.formatThrowable(cause))
+    }
+  }
+
+  class ReadEntireFileError(override val msg: String)
+    extends TestError(msg) {
+    def this(f: File, cause: Throwable) {
+      this(s"Error reading file < $f >: " +
+        SqlppError.formatThrowableCause(cause))
     }
   }
 
